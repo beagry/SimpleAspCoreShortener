8b376955e9332a0c52fd985d96f4ebccc3691ed6
Denis Kozlov <dkozlov@bytewerk.com>
2016-12-03 14:29:25 +0300

    simple shortener page + change default page to shortener page

---
+++ ClientApp/app/components/shortener/shortener.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'shortener',
+    templateUrl: './shortener.component.html',
+    styleUrls: ['./shortener.component.css']
+})
+
+export class ShortenerComponent {
+
+}
