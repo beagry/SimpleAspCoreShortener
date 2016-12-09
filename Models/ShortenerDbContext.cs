1ce7e16c12f646892553abc2313e13c623318b77
deniskozlov <deniskozlov@DENISKOZLOV8C65>
2016-12-09 11:57:31 +0300

    drop create database on start

--- Models/ShortenerDbContext.cs
+++ Models/ShortenerDbContext.cs
@@ -4,10 +4,26 @@ namespace SimpleUrlShortenerSPA.Models
 {
     public class ShortenerDbContext : DbContext 
     {
-        public ShortenerDbContext(){ }
+        private static bool created = false;
+        public ShortenerDbContext()
+        {
+            if (!created)
+            {
+                Database.EnsureDeleted();
+                Database.EnsureCreated();
+                created = true;
+            }
+         }
         public ShortenerDbContext(DbContextOptions<ShortenerDbContext> options) 
         : base(options)
-        { }
+        {
+            if (!created)
+            {
+                Database.EnsureDeleted();
+                Database.EnsureCreated();
+                created = true;
+            }
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
