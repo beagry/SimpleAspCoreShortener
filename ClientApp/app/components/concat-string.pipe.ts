c2abefe79d57291d5d9bb43106b50ce5c6fba51a
deniskozlov <deniskozlov@DENISKOZLOV8C65>
2016-12-13 01:13:37 +0300

    fix web-app crash on open history page (sry, only hardcode yet)

--- ClientApp/app/components/concat-string.pipe.ts
+++ ClientApp/app/components/concat-string.pipe.ts
@@ -3,6 +3,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name:'concat'})
 export class ConcatStringPipe implements PipeTransform {
     transform(value: string, args?: any) :string {
-        return window.location.origin + "/u" + value;
+        return "http://shrt1.azurewebsites.net" + "/u" + value; //  "http://localhost:5000" + "/u" + value; 
     }
 }
